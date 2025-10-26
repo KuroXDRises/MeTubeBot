8d58a198c54f9432c9ed2abbc664f4d3454d55dc
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-10-26 20:02:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from pyrogram import Client
 import start, register
-MeTube = Client("MeTube",
-                api_id=27548865,
-                api_hash="db07e06a5eb288c706d4df697b71ab61",
-                bot_token="8304647918:AAEmLuWW9mXpyfygdUpOdAd0PxSiqgZXllI"
-               )
+from bot import MeTube
 if __name__=="__main__":
     MeTube.run()
     print("MeTubeBot Starting")

df782edc11fc71a2487d4890a0d0d651ad701cb4
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-10-26 19:56:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ MeTube = Client("MeTube",
                )
 if __name__=="__main__":
     MeTube.run()
+    print("MeTubeBot Starting")
     with MeTube:
         MeTube.send_message(6239769036, "Bot Started 💯")

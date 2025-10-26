5a4f884b99ca59448998fe7b234b58a341a20176
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-10-26 19:54:12 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from db import *
 from pyrogram.enums import ParseMode
+from setup import Metube
 @MeTube.on_message(filters.command("start"), group=1)
 async def start(client, message):
     user = message.from_user

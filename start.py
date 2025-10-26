6315ba6a02e7714d2d9e2e7d0a3fdc77ee7d42fc
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-10-26 16:29:30 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+from db import *
+
+@MeTube.on_message(filters.command("start"), group=1)
+async def start(client, message):
+    user = message.from_user
+
+    await message.reply_photo(
+        photo="https://files.catbox.moe/vomj5q.jpg",
+        caption=f"👋 **Hello {user.first_name}!**\n\n"
+                "Welcome to **MeTube** 🎥\n"
+                "Here you can download anime clips, episodes & more!\n\n"
+                "🔥 Send me **link** or **anime name** to start."
+    )

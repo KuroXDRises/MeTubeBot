93655589efa99a12806021eda10cc343b2086efb
Kᴜʀᴏ Rᴀɪᴊɪɴ ("⁧;(⁧ <sufyan532011@gmail.com>
2025-10-28 16:49:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,5 +2,5 @@ from pyrogram import Client
 MeTube = Client("MeTube",
                 api_id=27548865,
                 api_hash="db07e06a5eb288c706d4df697b71ab61",
-                bot_token="8304647918:AAEmLuWW9mXpyfygdUpOdAd0PxSiqgZXllI"
+                bot_token="8304647918:AAE9PPOIB-qhAaQchkwGMTVqgf-WTvujHa8"
                )
